681d3948b6407bef290d729430e35dc39c5720a8
jin2719 <cimaphj3@gmail.com>
2025-03-30 13:23:29 -0700

    Update leak.cpp
    add exception

--- Projects/leak.cpp
+++ Projects/leak.cpp
@@ -3,13 +3,13 @@
 * Author: Hyeonjin Park
 * Purpose: Fixing Leak
 *******************************************************************************/
-
-#include "choices.h"
-#include "person.h"
+#include <choices.h>
+#include <person.h>
 #include <iostream>
 #include <ctime>
 #include <cstdlib>
 #include <string>
+#include <limits>
 
 using namespace std;
 
@@ -19,7 +19,6 @@ void fixLeak() {
     cout << "As you find the leak, you realized that you don't have tools to fix the leak." << endl;
     cout << "You need to communicate with the station to get some advice..." << endl;
     cout << "Go to fix the communication and come back!" << endl;
-    
     
     /*redirect to fix comms first
     fixCommunication();
@@ -39,40 +38,53 @@ void fixLeak() {
     
     srand(time(0));
     
-
     while (true) {
-        cout << "Enter your choice => ";
-        cin >> userC;
+        try {
+            cout << "Enter your choice => ";
+            cin >> userC;
+            
+            const string ERROR = "ERROR: Invalid option. Try again...";
+
+	        // If user input wrong option, user can input again until it's valid
+	        if (userC != "rock" && userC != "scissors" && userC != "paper") {
+	            throw ERROR;
+	        }
         
-        // random choice between rock, scissors, and paper
-        int randomChoice = rand() % 3;
+            // random choice between rock, scissors, and paper
+            int randomChoice = rand() % 3;
         
-        computerC = threeChoice[randomChoice];
-        cout << "Program choice => " << computerC << endl;
+            computerC = threeChoice[randomChoice];
+            cout << "Program choice => " << computerC << endl;
         
-        /* if user and computer are tie, try again
-        if user wins, complete fixing leak
-        if user lose, try again
-	*/
-	
-        if (userC == computerC) {
-            cout << "Tie!" << endl;
-        } else if ((userC == "rock" && computerC == "scissors") ||
-                   (userC == "scissors" && computerC == "paper") ||
-                   (userC == "paper" && computerC == "rock")) {
-            cout << "You won! Great job!" << endl;
-	    cout << "You fixed the leak." << endl;
-            return;
-        } else {
-            cout << "You lost! Try one more time..." << endl;
+            /* if user and computer are tie, try again
+            if user wins, complete fixing leak
+            if user lose, try again
+	        */
+
+            if (userC == computerC) {
+                cout << "Tie!" << endl;
+            } else if ((userC == "rock" && computerC == "scissors") ||
+                    (userC == "scissors" && computerC == "paper") ||
+                    (userC == "paper" && computerC == "rock")) {
+                cout << "You won! Great job!" << endl;
+	        cout << "You fixed the leak." << endl;
+                return;
+            } else {
+                cout << "You lost! Try one more time..." << endl;
+            }
+            
+
+        } catch (const string& error) {
+            cout << error << endl;
+            cin.clear();
+            cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            continue; // prompt user to try again.
         }
-
-	// If user input wrong option, user can input again until it's valid
-	if (userC != "rock" && userC != "scissors" && userC != "paper") {
-	    cout << "Invalid option. Try again..." << endl;
-	}
-
     }
 }
 
+int main() {
+    fixLeak();
+    return 0;
+}
     
