b2db889d6397790ad3e1bd4d8011950e9c133f34
Dev <trujild3@my.erau.edu>
2025-03-30 18:09:35 -0700

    Update game.cpp
    additional comments on what part of person.h will go into choices.h

--- Projects/game.cpp
+++ Projects/game.cpp
@@ -7,9 +7,11 @@ int main(){
 /*Startup.h*/
     Start s;
     s.displayStart();
-/*person.h*/
+    
+/*person.h*/ /* This would be in choices.h*/
     UserStatus player(100, 100, 100); //Sets initial status values
     player.displayStatus(); //Displays initial status values
+    
     char choice; // Char for user choice option
     while (true) { // While a user still has health
         cout << "choice a/b/c: "; //Prompt choice to choose
