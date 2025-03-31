79961aa199a9be9f14aaee59478f5fa7fd4623e2
InterstellarDev <57544870+InterstellarDev7@users.noreply.github.com>
2025-03-30 23:42:34 -0700

    Move choices from game.cpp to header file. Also added a draft of the switch case for choices

--- Projects/game.cpp
+++ Projects/game.cpp
@@ -10,20 +10,6 @@ int main(){
 /*person.h*/
     UserStatus player(100, 100, 100); //Sets initial status values
     player.displayStatus(); //Displays initial status values
-    char choice; // Char for user choice option
-    while (true) { // While a user still has health
-        cout << "choice a/b/c: "; //Prompt choice to choose
-        cin >> choice; //Take user input
-
-        if (choice == 'a' || choice == 'b'|| choice =='c') { // If a choice is chosen
-            if (!player.makeChoice()) { //Once user runs out of health
-                cout << "Game Over! \nThank you for Playing!" << endl; //End message
-                break; //Stop code 
-            }
-        } else { //Error handling
-            cout << "Invalid choice. Please enter 'a' 'b' or 'c'." << endl; //Error message and reprompt
-        }
-    }
 
     return 0;
 }
