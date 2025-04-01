475338363a8f07226d4501b50520f470f08cec4b
Dev <trujild3@my.erau.edu>
2025-03-31 19:00:48 -0700

    Create randomTasks.h
    randomTasks.cpp functions stored here to be called in choices.cpp

---
+++ Projects/randomTasks.h
@@ -0,0 +1,17 @@
+#ifndef RANDOMTASKS_H
+#define RANDOMTASKS_H
+
+void sleep();
+void eatFood();
+void readBook();
+void scrollYouTube();
+void listenMusic();
+void watchMovie();
+void familyPicture();
+void liveStream();
+void tweet();
+void makeTikTok();
+void victoryMeal();
+void playDrums();
+
+#endif
