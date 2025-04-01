5dabea006e8418cc4a705c6eb4cf53d05ebd6e43
Dev <trujild3@my.erau.edu>
2025-04-01 11:26:04 -0700

    Update choices.h
    Working choices.h file, had to fix errors and make changes with the new person.h

--- Projects/choices.h
+++ Projects/choices.h
@@ -1,84 +1,103 @@
-#ifndef CHOICES_H 
-#define CHOICES_H 
+#ifndef CHOICES_H
+#define CHOICES_H
 
 #include <iostream>
 #include <fstream>
-#include "person.h"
 #include "power.h"
 #include "communication.h"
 #include "leak.h"
 #include "randomTasks.h"
+#include "person.h" // Include person.h to use UserStatus
 using namespace std;
 
 class Choices {
 private:
-    char choice;
-    int level = 1;
+    char choice; // Char for user choice option
+    int level = 1; // Level of the game
+    UserStatus &player; // Store reference to UserStatus object
+
+    // Error handling function (fixes the missing function issue)
+    char errorHandling() {
+        cout << "Invalid input! Please enter a valid choice.\n";
+        char newChoice;
+        cin >> newChoice;
+        return newChoice;
+    }
 
 public:
-    /* Constructor */
-    Choices(UserStatus &player){
-        player.displayStatus();
+    /* Constructor initializes user status */
+    Choices(UserStatus &playerRef) : player(playerRef) {
+        player.displayStatus(); // Displays initial status values
     }
 
-    /* Destructor */
-    ~Choices(){
-        cout << "You can not make any choices anymore." << endl;
+    // Destructor
+    ~Choices() {
+        cout << "You can no longer make choices..." << endl;
     }
 
-    /* Function to handle invalid input */
-    char errorHandling(){
-        cout << "Invalid choice. Please enter 'a' 'b' 'c' 'd' 'e' or 'f'." << endl;
-        cin >> this->choice;
-        return this->choice;
+    /* Function to write a congratulations certificate */
+    void writeCertificate() {
+        ofstream outFile("Congratulations_Certificate.txt"); // Create and open a file
+        if (outFile.is_open()) {
+            outFile << "***************************************\n";
+            outFile << "          CONGRATULATIONS!             \n";
+            outFile << "***************************************\n";
+            outFile << "You have successfully completed the game!\n";
+            outFile << "Your hard work and determination have\n";
+            outFile << "saved the ISS and its crew. Great job!\n";
+            outFile << "***************************************\n";
+            outFile << "          CERTIFICATE OF SUCCESS       \n";
+            outFile << "***************************************\n";
+            outFile.close(); // Close the file
+            cout << "Congratulations! A certificate has been written to 'Congratulations_Certificate.txt' file.\n";
+        } else {
+            cout << "Error writing to file!" << endl;
+        }
     }
 
     /* Overloaded ++ operator */
-    Choices& operator++(){
-        level++;
-        return *this;
+    void operator++() {
+        level++; // Increment level
     }
 
-    /* Function to select choices */
-    void selectChoice(UserStatus &player) {
-        while (player.makeChoice() && level < 5) {
+    /* Function to display and choose a choice */
+    void selectChoice() { // No need to pass player since it's stored
+        while (player.makeChoice() && level < 5) { // While user has health or game is not over
             switch (level) {
-                case 1:
-                    cout << "Choices\n(a) Fix power\n(b) Fix communcations\n(c) Fix leak\n(d) Keep sleeping\n(e) Eat some food\n(f) Read a book\n";
+                case 1: // Beginning of the game
+                    cout << "Choices\n(a) Fix power\n(b) Fix communications\n(c) Fix leak\n(d) Keep sleeping\n(e) Eat some food\n(f) Read a book\n";
                     cin >> choice;
 
-                    if (choice == 'a' || choice == 'b' || choice == 'c' || choice == 'd' || choice == 'e' || choice == 'f') {
-                        if (choice == 'a') {
-                            fixPower();
-                            ++(*this);
-                        } else if (choice == 'b') {
-                            cout << "You can not fix communications without power yet!" << endl;
-                        } else if (choice == 'c') {
-                            cout << "You can not fix the leak yet!" << endl;
-                        } else if (choice == 'd') {
-                            sleep();
-                        } else if (choice == 'e') {
-                            eatFood();
-                            player.setUserHunger(player.getUserHunger() + 10);
-                        } else if (choice == 'f') {
-                            readBook();
-                        }
+                    if (choice == 'a') {
+                        fixPower();
+                        level++;
+                    } else if (choice == 'b') {
+                        cout << "You cannot fix communications without power yet!" << endl;
+                    } else if (choice == 'c') {
+                        cout << "You cannot fix the leak yet!" << endl;
+                    } else if (choice == 'd') {
+                        sleep();
+                    } else if (choice == 'e') {
+                        eatFood();
+                        player.setUserHunger(player.getUserHunger() + 10);  // Increase hunger by 10
+                    } else if (choice == 'f') {
+                        readBook();
                     } else {
                         choice = errorHandling();
                     }
                     player.displayStatus();
                     break;
 
-                case 2:
+                case 2: // After fixing power
                     cout << "Choices\n(a) Fix communications\n(b) Fix leak\n(c) Scroll on YouTube\n(d) Listen to music\n(e) Watch a movie\n";
                     cin >> choice;
 
                     if (choice == 'a') {
                         cout << "You turn on the main power switch inside the ISS." << endl;
                         startCommunicationMiniGame();
-                        ++(*this);
+                        level++;
                     } else if (choice == 'b') {
-                        cout << "You can not fix the leak without communications!" << endl;
+                        cout << "You cannot fix the leak without communications!" << endl;
                     } else if (choice == 'c') {
                         scrollYouTube();
                     } else if (choice == 'd') {
@@ -91,13 +110,13 @@ public:
                     player.displayStatus();
                     break;
 
-                case 3:
-                    cout << "Choices\n(a) Call JPL \n(b) Video call your family\n(c) Livestream on Twitch\n(d) Post a tweet\n";
+                case 3: // After fixing communications
+                    cout << "Choices\n(a) Call JPL\n(b) Video call your family\n(c) Livestream on Twitch\n(d) Post a tweet\n";
                     cin >> choice;
 
                     if (choice == 'a') {
                         cout << "You call JPL and they give you instructions on how to fix the leak." << endl;
-                        ++(*this);
+                        level++;
                     } else if (choice == 'b') {
                         familyPicture();
                     } else if (choice == 'c') {
@@ -110,14 +129,15 @@ public:
                     player.displayStatus();
                     break;
 
-                case 4:
+                case 4: // Before fixing leak
                     cout << "Choices\n(a) Fix leak\n(b) Make a TikTok\n(c) Eat a victory meal\n(d) Play around with the wrench\n";
                     cin >> choice;
 
                     if (choice == 'a') {
                         fixLeak();
+                        writeCertificate();
                         cout << "You have successfully completed the game! Well done!" << endl;
-                        return;
+                        return; // END GAME
                     } else if (choice == 'b') {
                         makeTikTok();
                     } else if (choice == 'c') {
@@ -130,9 +150,9 @@ public:
                     }
                     player.displayStatus();
                     break;
-            }
-        }
-    }
-};
+            } // End of switch statement
+        } // End of while loop
+    } // End of selectChoice() function
+}; // End of class
 
 #endif
