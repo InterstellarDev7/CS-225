ce1ab2e29196464023928415667463e75374b69c
Dev <trujild3@my.erau.edu>
2025-03-31 18:57:59 -0700

    Update person.h
    took out display and added header safe guards

--- Projects/person.h
+++ Projects/person.h
@@ -3,30 +3,45 @@
 * Author:   Devin Trujillo
 * Purpose:  User Status: Health, Oxygen, and Hunger
 *******************************************************************************/
+#ifndef PERSON_H 
+#define PERSON_H 
+
 #include <iostream>
 #include <cstring>
 using namespace std;
 
-class UserStatus{
-    private:
-        int userHunger;
-        int userOxygen;
-        int userHealth;
-    public:
-        UserStatus(int hu, int ox, int he):userHunger(hu), userOxygen(ox), userHealth(he){} //Constructor Sets initial values
-        void displayStatus();
-        bool makeChoice();
+class UserStatus {
+private:
+    int userHunger;
+    int userOxygen;
+    int userHealth;
+
+public:
+    UserStatus(int hu, int ox, int he) : userHunger(hu), userOxygen(ox), userHealth(he) {} // Constructor sets initial values
+
+    // Getter functions
+    int getUserHunger() const { return userHunger; }
+    int getUserOxygen() const { return userOxygen; }
+    int getUserHealth() const { return userHealth; }
+
+    // Setter functions
+    void setUserHunger(int hunger) { userHunger = hunger; }
+    void setUserOxygen(int oxygen) { userOxygen = oxygen; }
+    void setUserHealth(int health) { userHealth = health; }
+
+    void displayStatus();
+    bool makeChoice();
 };
 
 /*
 Function: displayStatus()
 This function is used to display the Game Stats for user to see
 */
-void UserStatus::displayStatus(){
-    cout<<"***************************************"<<endl;
-    cout<<"Game Stats: "<<endl<<"Hunger = "<<userHunger<<"\nOxygen = "<<userOxygen
-    <<"\nHealth = "<<userHealth<<endl;
-    cout<<"***************************************"<<endl;
+void UserStatus::displayStatus() {
+    cout << "***************************************" << endl;
+    cout << "Game Stats: " << endl << "Hunger = " << userHunger << "\nOxygen = " << userOxygen
+         << "\nHealth = " << userHealth << endl;
+    cout << "***************************************" << endl;
 }
 
 /*
@@ -41,25 +56,22 @@ bool UserStatus::makeChoice() {
     userHunger = max(0, userHunger - 2); // Max keeps stats from going below zero, decreases by 2
     userOxygen = max(0, userOxygen - 2); // Max keeps stats from going below zero, decreases by 2
 
-    if (userOxygen <= 50 ) {
+    if (userOxygen <= 50) {
         userHealth = max(0, userHealth - 15); // Max keeps stats from going below zero, decreases by 15
         cout << "You have less than 50 percent Oxygen left! Health now drops by 15." << endl;
-    } 
-    
-    if (userHunger <= 30 ) {
+    }
+
+    if (userHunger <= 30) {
         userHealth = max(0, userHealth - 10); // Max keeps stats from going below zero, decreases by 10
         cout << "You have less than 40 percent Hunger left! Health now drops by 10." << endl;
     }
-    
+
     if (userHealth == 0) {
         displayStatus();
         cout << "You have run out of health :(" << endl;
-        return false; //No more health means no more choices can be made, game ends
+        return false; // No more health means no more choices can be made, game ends
     }
-
-    cout << "Stats updated." << endl;
-    displayStatus();
-    return true; //Still some health so more choices can be made
+    cout<<"\n";
+    return true; // Still some health so more choices can be made
 }
-
-
+#endif
