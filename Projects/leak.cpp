3fce18d9af117733d08bfee32040c8a4b864cea7
Dev <trujild3@my.erau.edu>
2025-03-31 19:03:46 -0700

    Update leak.cpp
    Took out code that checks for if the power was fixed first. Added winning ascii art

--- Projects/leak.cpp
+++ Projects/leak.cpp
@@ -3,82 +3,66 @@
 * Author: Hyeonjin Park
 * Purpose: Fixing Leak
 *******************************************************************************/
-#include <choices.h>
-#include <person.h>
 #include <iostream>
 #include <ctime>
 #include <cstdlib>
 #include <string>
 #include <limits>
-
+#include "leak.h"
 using namespace std;
 
 void fixLeak() {
-    // user chose to fix leak first
-    cout << "You decided to fix the leak." << endl;
-    cout << "As you find the leak, you realized that you don't have tools to fix the leak." << endl;
-    cout << "You need to communicate with the station to get some advice..." << endl;
-    cout << "Go to fix the communication and come back!" << endl;
-    
-    /*redirect to fix comms first
-    fixComms communication;
-    communication.playTicTacToe();
-    
-    // return to fixing the leak -> mini game
-    cout << "\n\nNow communication is fixed." << endl;
-    cout << "You contacted the station and got advice on how to fix the leak." << endl;
     cout << "To fix the leak, you need to complete the mini game!" << endl;
     cout << "Let's start the game!" << endl;
-    cout << "*********Welcome to Rock, Scissors, Paper*********" << endl;
-    cout << "To fix the leak, you must win a game." << endl;
-    
+    cout << "\n*********Welcome to Rock, Scissors, Paper*********" << endl;
+    cout << "To fix the leak, you must win a game.\n" << endl;
     string threeChoice[3] = {"rock", "scissors", "paper"};
     string userC;
     string computerC;
     
     srand(time(0));
     
-    while (true) {
-        try {
-            cout << "Enter your choice => ";
+    try {
+        while (true) {
+            cout << "Enter your choice ('rock', 'scissors', or 'paper' => ";
             cin >> userC;
             
-            const string ERROR = "ERROR: Invalid option. Try again...";
+            const string ERROR = "ERROR: Invalid option. Try again...\n";
 
-	        // If user input wrong option, user can input again until it's valid
-	        if (userC != "rock" && userC != "scissors" && userC != "paper") {
-	            throw ERROR;
-	        }
-        
+            // If user input wrong option, user can input again until it's valid
+            if (userC != "rock" && userC != "scissors" && userC != "paper") {
+                throw ERROR;
+            }
+    
             // random choice between rock, scissors, and paper
             int randomChoice = rand() % 3;
-        
+    
             computerC = threeChoice[randomChoice];
-            cout << "Program choice => " << computerC << endl;
-        
-            /* if user and computer are tie, try again
-            if user wins, complete fixing leak
-            if user lose, try again
-	        */
-
+            cout << "Program choice => " << computerC <<endl<<endl;
+    
+            // if user and computer are tie, try again
             if (userC == computerC) {
                 cout << "Tie!" << endl;
             } else if ((userC == "rock" && computerC == "scissors") ||
                     (userC == "scissors" && computerC == "paper") ||
                     (userC == "paper" && computerC == "rock")) {
                 cout << "You won! Great job!" << endl;
-	        cout << "You fixed the leak." << endl;
+                cout << "You fixed the leak!" << endl;
+                cout << "      *****    " << endl;
+				cout << "    *       *  " << endl;
+				cout << "   *  LEAK   * " << endl;
+				cout << "   *  SEALED * " << endl;
+				cout << "    *       *  " << endl;
+				cout << "      *****    " << endl;
+				cout<<"\n";
                 return;
             } else {
                 cout << "You lost! Try one more time..." << endl;
             }
-            
-
-        } catch (const string& error) {
-            cout << error << endl;
-            cin.clear();
-            cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-            continue; // prompt user to try again.
         }
+    } catch (const string& error) {
+        cout << error << endl;
+        cin.clear();
+        cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
     }
 }
