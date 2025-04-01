ad41240c98aa02d4970060cf28147226779d34a5
InterstellarDev <57544870+InterstellarDev7@users.noreply.github.com>
2025-04-01 14:33:08 -0700

    Updated choicess.h

--- Projects/choices.h
+++ Projects/choices.h
@@ -66,6 +66,7 @@ public:
         while(player.makeChoice() && level < 5){ // While user has health or game is not over
             switch(level){
                 case 1: // Beginning of the game
+                    this_thread::sleep_for(chrono::seconds(2));
                     cout << "Choices\n(a) Fix power\n(b) Fix communications\n(c) Fix leak\n(d) Keep sleeping\n(e) Eat some food\n(f) Read a book\n";
                     cin >> choice;
 
@@ -97,6 +98,7 @@ public:
                     break;
 
                 case 2: // After fixing power
+                    this_thread::sleep_for(chrono::seconds(2));
                     cout << "Choices\n(a) Fix communications\n(b) Fix leak\n(c) Scroll on YouTube\n(d) Listen to music\n(e) Watch a movie\n";
                     cin >> choice;
 
@@ -125,6 +127,7 @@ public:
                     break;
 
                 case 3: // After fixing communications
+                    this_thread::sleep_for(chrono::seconds(2));
                     cout << "Choices\n(a) Call JPL\n(b) Video call your family\n(c) Livestream on Twitch\n(d) Post a tweet\n";
                     cin >> choice;
 
@@ -149,6 +152,7 @@ public:
                     break;
 
                 case 4: // Before fixing leak
+                    this_thread::sleep_for(chrono::seconds(2));
                     cout << "Choices\n(a) Fix leak\n(b) Make a TikTok\n(c) Eat a victory meal\n(d) Play around with the wrench\n";
                     cin >> choice;
 
